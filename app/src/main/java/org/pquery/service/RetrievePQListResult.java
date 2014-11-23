1aacfb8970ad63e2a259255d14c8929673ecc229
Peter Storch <peter.storch@gmail.com>
2014-11-23 22:42:57 +0100

    Initial commit

---
+++ app/src/main/java/org/pquery/service/RetrievePQListResult.java
@@ -0,0 +1,20 @@
+package org.pquery.service;
+
+import org.pquery.dao.PQ;
+import org.pquery.webdriver.FailurePermanentException;
+
+public class RetrievePQListResult {
+    public FailurePermanentException failure;
+    public PQ[] pqs = null;
+    public PQ[] repeatables = null;
+    
+    public RetrievePQListResult(FailurePermanentException failure) {
+        this.failure = failure;
+    }
+    public RetrievePQListResult(PQ[] pqs, PQ[] repeatables) {
+        this.pqs = pqs;
+        this.repeatables = repeatables;
+    }
+    public RetrievePQListResult() {
+    }
+}
